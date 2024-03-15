ba4f518b1a4b65c06a9c4166fd9abc472d2a1ba1
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-03-15 22:48:30 +0400

    Initial comment: Creating emp-dept-connect project

---
+++ emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/EmpDeptConnectApplication.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.empdeptconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpDeptConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpDeptConnectApplication.class, args);
+	}
+
+}
---
+++ emp-dept-connect/src/test/java/az/edu/ada/wm2/empdeptconnect/EmpDeptConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.empdeptconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpDeptConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
