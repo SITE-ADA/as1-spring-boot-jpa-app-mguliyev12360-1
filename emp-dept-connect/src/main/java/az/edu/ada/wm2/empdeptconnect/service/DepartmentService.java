b8e6c4325360dcdccf6e8c7424bf949462705b8d
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-03-16 20:48:19 +0400

    DepartmentService and EmployeeService interfaces are added to service. Also impl packet created in service which contains DepartmentServiceImpl and EmployeeServiceImpl classes

---
+++ emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/service/DepartmentService.java
@@ -0,0 +1,4 @@
+package az.edu.ada.wm2.empdeptconnect.service;
+
+public interface DepartmentService {
+}
---
+++ emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,4 @@
+package az.edu.ada.wm2.empdeptconnect.service.impl;
+
+public class DepartmentServiceImpl {
+}
