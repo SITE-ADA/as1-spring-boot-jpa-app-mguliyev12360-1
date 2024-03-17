9ea160b6e225385b6fcac38a6ba96466b59b38ab
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-03-17 07:00:26 +0400

    Department class is done in model and some minor changes happened in project inside

--- emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/model/Department.java
+++ emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/model/Department.java
@@ -1,4 +1,28 @@
 package az.edu.ada.wm2.empdeptconnect.model;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+
+@ToString
+
+@Table(name = "departments")
 public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Employee> employees = new HashSet<>();
+
 }
--- emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/service/impl/DepartmentServiceImpl.java
+++ emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/service/impl/DepartmentServiceImpl.java
@@ -1,29 +1,4 @@
-package az.edu.ada.wm2.empdeptconnect.model;
+package az.edu.ada.wm2.empdeptconnect.service.impl;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
-
-@Entity
-@Table(name = "employees")
-public class Employee {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String firstName;
-    private String lastName;
-    private Double salary;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "department_id")
-    private Department department;
-
+public class DepartmentServiceImpl {
 }
-
