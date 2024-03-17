8b8d46c04409c20473a19863bc22a5c56f162345
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-03-18 00:43:55 +0400

    DepartmentRepository interface is ready in Repository

--- emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/repository/DepartmentRepository.java
+++ emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/repository/DepartmentRepository.java
@@ -1,4 +1,34 @@
 package az.edu.ada.wm2.empdeptconnect.repository;
 
-public interface DepartmentRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import az.edu.ada.wm2.empdeptconnect.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    //    Query Methods
+    List<Department> findByNameContainingIgnoreCase(String name);
+
+
+    List<Department> findByName(String name);
+
+
+    //    Non-Native Query
+    @Query("SELECT d FROM Department d ORDER BY d.name")
+    List<Department> findAllDepartmentsOrderedByName();
+
+
+    //    Filter
+    Page<Department> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
+
+
 }
+
