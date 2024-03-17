270226a823d50320f966ef9b10e95f33b8a507a9
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-03-17 06:48:44 +0400

    Department class is done in model

--- emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/service/impl/DepartmentServiceImpl.java
+++ emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/service/impl/DepartmentServiceImpl.java
@@ -1,4 +1,29 @@
-package az.edu.ada.wm2.empdeptconnect.service.impl;
+package az.edu.ada.wm2.empdeptconnect.model;
 
-public class DepartmentServiceImpl {
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private Double salary;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
 }
+
