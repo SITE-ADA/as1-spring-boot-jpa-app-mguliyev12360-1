c49b281071eb0dc41d5bf11561c282c0fc1f4892
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-03-17 14:11:05 +0400

    Employee class is done in Model

--- emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/model/Employee.java
+++ emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/model/Employee.java
@@ -1,4 +1,29 @@
 package az.edu.ada.wm2.empdeptconnect.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private Double salary;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
 }
+
