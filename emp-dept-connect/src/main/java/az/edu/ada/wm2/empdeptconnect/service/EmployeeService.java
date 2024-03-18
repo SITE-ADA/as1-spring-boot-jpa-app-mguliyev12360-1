731cc83f317db41f43a74b09f0d2b81449e29135
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-03-19 02:35:17 +0400

    EmployeeService interface is completed in Service

--- emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/service/EmployeeService.java
+++ emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/service/EmployeeService.java
@@ -1,4 +1,29 @@
 package az.edu.ada.wm2.empdeptconnect.service;
 
+
+import az.edu.ada.wm2.empdeptconnect.model.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface EmployeeService {
+    List<Employee> getAll();
+    Employee getById(Long id);
+    Employee save(Employee employee);
+    void delete(Long id);
+
+    List<Employee> findByFirstName(String lastName);
+    List<Employee> findByLastName(String lastName);
+    List<Employee> findEmployeesWithSalaryGreaterThan(Double salary);
+
+
+
+    Page<Employee> findAllEmployees(Pageable pageable);
+
+
+    Page<Employee> findByLastNameContainingIgnoreCase(String lastName, Pageable pageable);
+
+
+
 }
