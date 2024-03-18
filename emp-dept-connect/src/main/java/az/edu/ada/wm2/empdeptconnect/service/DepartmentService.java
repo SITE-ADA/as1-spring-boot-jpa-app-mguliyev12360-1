ed7cec5c392730cefbf9a74ca16fe589374ab4e3
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-03-18 20:54:48 +0400

    DepartmentService interface is completed in Service

--- emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/service/DepartmentService.java
+++ emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/service/DepartmentService.java
@@ -1,4 +1,34 @@
 package az.edu.ada.wm2.empdeptconnect.service;
 
+import az.edu.ada.wm2.empdeptconnect.model.Department;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface DepartmentService {
+    List<Department> getAll();
+    Department getById(Long id);
+    Department save(Department department);
+    void delete(Long id);
+
+
+
+    List<Department> findByNameContainingIgnoreCase(String name);
+    List<Department> findAllDepartmentsOrderedByName();
+
+    List<Department> findByName(String name);
+
+
+
+    Page<Department> findAllDepartments(Pageable pageable);
+
+
+
+    Page<Department> findByNameContainingIgnoreCasePageable(String name, Pageable pageable);
+
+
+
+
+
 }
