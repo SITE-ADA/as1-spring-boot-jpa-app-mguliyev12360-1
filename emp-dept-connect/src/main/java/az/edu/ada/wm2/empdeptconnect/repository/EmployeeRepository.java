44a3487a900e9fc1e44a0cf02b5bb04774c25f2b
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-03-18 11:03:24 +0400

    EmployeeRepository interface is ready in Repository

--- emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/repository/EmployeeRepository.java
+++ emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/repository/EmployeeRepository.java
@@ -1,4 +1,33 @@
 package az.edu.ada.wm2.empdeptconnect.repository;
 
-public interface EmployeeRepository {
+
+import az.edu.ada.wm2.empdeptconnect.model.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    //    Query Methods
+    List<Employee> findByFirstName(String lastName);
+
+    List<Employee> findByLastName(String lastName);
+
+
+    //    Non-Native Query
+    @Query("SELECT e FROM Employee e WHERE e.salary > :salary")
+    List<Employee> findEmployeesWithSalaryGreaterThan(@Param("salary") Double salary);
+
+
+    //    Filter
+    Page<Employee> findByLastNameContainingIgnoreCase(String lastName, Pageable pageable);
+
+
+
 }
