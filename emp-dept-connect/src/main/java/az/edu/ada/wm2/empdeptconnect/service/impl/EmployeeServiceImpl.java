c1b5ce4e69f4d64b2be12d1455cdf2adb35a1c1d
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-03-20 02:16:40 +0400

    EmployeeServiceImpl is done in impl package located in service

--- emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/service/impl/EmployeeServiceImpl.java
+++ emp-dept-connect/src/main/java/az/edu/ada/wm2/empdeptconnect/service/impl/EmployeeServiceImpl.java
@@ -1,4 +1,72 @@
 package az.edu.ada.wm2.empdeptconnect.service.impl;
 
-public class EmployeeServiceImpl {
+
+import az.edu.ada.wm2.empdeptconnect.model.Employee;
+import az.edu.ada.wm2.empdeptconnect.repository.EmployeeRepository;
+import az.edu.ada.wm2.empdeptconnect.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> getAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public void delete(Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+
+    @Override
+    public List<Employee> findByFirstName(String firstName) {
+        return employeeRepository.findByFirstName(firstName);
+    }
+    @Override
+    public List<Employee> findByLastName(String lastName) {
+        return employeeRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public List<Employee> findEmployeesWithSalaryGreaterThan(Double salary) {
+        return employeeRepository.findEmployeesWithSalaryGreaterThan(salary);
+    }
+
+
+
+    @Override
+    public Page<Employee> findAllEmployees(Pageable pageable) {
+        return employeeRepository.findAll(pageable);
+    }
+
+
+
+    @Override
+    public Page<Employee> findByLastNameContainingIgnoreCase(String lastName, Pageable pageable) {
+        return employeeRepository.findByLastNameContainingIgnoreCase(lastName, pageable);
+    }
+
 }
