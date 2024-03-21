a29748e0a2b3c50a541fbc1b7e3ea4d5aff3733e
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-03-22 02:53:50 +0400

    data.sql is created and completed in resources

---
+++ emp-dept-connect/src/main/resources/data.sql
@@ -0,0 +1,31 @@
+-- Inserting departments
+INSERT INTO departments (name) VALUES ('Human Resources');
+INSERT INTO departments (name) VALUES ('IT');
+INSERT INTO departments (name) VALUES ('Finance');
+INSERT INTO departments (name) VALUES ('Marketing');
+INSERT INTO departments (name) VALUES ('Operations');
+INSERT INTO departments (name) VALUES ('Customer Service');
+INSERT INTO departments (name) VALUES ('Research and Development');
+INSERT INTO departments (name) VALUES ('Legal');
+INSERT INTO departments (name) VALUES ('Sales');
+INSERT INTO departments (name) VALUES ('Human Capital');
+
+
+-- Inserting Employees,
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('John', 'Doe', 50000.0, 1);
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('Jane', 'Doe', 60000.0, 2);
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('Jim', 'Bean', 55000.0, 1);
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('Jill', 'Stein', 70000.0, 3);
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('Emily', 'Clark', 65000.0, 4);
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('Dylan', 'Smith', 58000.0, 5);
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('Liam', 'Johnson', 72000.0, 6);
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('Sophia', 'Lopez', 75000.0, 7);
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('Ethan', 'Martinez', 62000.0, 8);
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('Nora', 'Hendrix', 67000.0, 9);
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('Alex', 'Garcia', 69000.0, 10);
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('Olivia', 'Brown', 72000.0, 2);
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('Mason', 'Davis', 56000.0, 4);
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('Ava', 'Miller', 63000.0, 6);
+INSERT INTO employees (first_name, last_name, salary, department_id) VALUES ('Isabella', 'Wilson', 68000.0, 8);
+
+
